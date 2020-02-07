426453e323b33c093491e7d60494d1ad705d0e07
Brendan Goodenough <brendan@goodenough.nz>
2020-02-07 13:03:35 +0100

    Add curl order example

---
+++ curl/curl-order-example.sh
@@ -0,0 +1,7 @@
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "X-Crystallize-Access-Token-Id: [your-crystallize-token-id]" \
+  -H "X-Crystallize-Access-Token-Secret: [your-crystallize-token-secret]" \
+  --data '{ "query": "{ orders { get(id: \"[order-id]\") { total { gross } } } }" }' \
+  https://api.crystallize.com/nerdenough/orders
